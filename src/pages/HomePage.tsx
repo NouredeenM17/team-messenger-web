558df15f300df81c53541c409c842694f919e87c
Nouredeen Mahmoud <nourahm.17@gmail.com>
2024-04-08 15:30:46 +0300

    Full Login functionality And a basiclogin successful homepage Backend route changed, /api now included

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
+    const token = localStorage.getItem("tm-auth-token");
+    const userId = localStorage.getItem("tm-user-id");
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!token){
+            return navigate('/login');
+        }
+      }, []);
+    
+    const logout = () => {
+        localStorage.removeItem("tm-auth-token");
+        localStorage.removeItem("tm-user-id");
+        return navigate('/login');
+    }
+  
+    return (
+        <>
+            <div>Welcome user: { userId }</div>
+            <div onClick={ logout }>Logout</div>
+        </>
+    
   )
 }
 
